0014199c0a270044e8e05eeec3af5028f74a0a12
termul488-ai <termul488@gmail.com>
2026-01-11 19:42:57 +0700

    Update siwak.py

--- siwak.py
+++ siwak.py
@@ -52,8 +52,8 @@ while attemps < 100:
         attemps += 1
         continue
 
-url = input("\033[48;5;7m═⟩⟩ URL:\033[0m \033[32m\033[0m")
-ask = fade.pinkred()
+url = input("\033[48;5;4m═⟩⟩ Enter URL (e.g., http://example.com:\033[0m \033[32m\033[0m")
+ask = fade.pinkred('txt')
 print("\033[32mLoading......")
 
 async def increment_view_count(session):
