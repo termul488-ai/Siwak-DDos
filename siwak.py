5c4be8520f28823447294429aa1cd83d4137df2e
termul488-ai <termul488@gmail.com>
2026-01-11 22:43:38 +0700

    Update siwak.py

--- siwak.py
+++ siwak.py
@@ -46,7 +46,7 @@ async def increment_view_count(session):
                 print("\033[48;5;2mInfo target \033[0m \033[33m" +str(url)+ " \033[35work\033[0m")
             else:
                 time.sleep(0.01)
-                print("\033[38;5;2mInfo target \033[33m" +str(view_count)+ " \033[35work\033[0m")          
+                print("\033[38;5;2mInfo target \033[33m" +str(packet_count)+ " \033[35work\033[0m")          
     except aiohttp.ClientError as e:
                 time.sleep(0.01)
                 print("\033[48;5;1mInfo target \033[0m \033[38;5;3m" +str(url)+ "  \033[38;5;7mMaybe down!\033[0m")
